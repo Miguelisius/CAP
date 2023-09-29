78dbc1da44b198e6b9de8fc4fafc3ebf43af2824
Miguelisius <miguelgonzalezlopezz@gmail.com>
2023-09-29 16:45:30 +0200

    Graficas echas

--- P1/material/simple2.c
+++ P1/material/simple2.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 #define ARRAY_SIZE 2048
-#define NUMBER_OF_TRIALS 1
+#define NUMBER_OF_TRIALS 1000000
 
 /*
  * Statically allocate our arrays.  Compilers can
@@ -12,7 +13,8 @@ static double a[ARRAY_SIZE], b[ARRAY_SIZE], c;
 
 int main(int argc, char *argv[]) {
     int i,t;
-
+    struct timeval start_time, end_time;
+    float seconds = 0;
     double m = 1.0001;
 
     /* Populate A and B arrays */
@@ -20,13 +22,24 @@ int main(int argc, char *argv[]) {
         b[i] = i;
         a[i] = i+1;
     }
-
+    FILE *pf = fopen("tiempos_simple.txt", "w");
+    if (!pf)
+    {
+        if (pf)
+            fclose(pf);
+        return -1;
+    }
     /* Perform an operation a number of times */
-    for (t=0; t < NUMBER_OF_TRIALS; t++) {
+    for (t=100000; t < NUMBER_OF_TRIALS + 1; t+=100000) {
+        gettimeofday(&start_time, NULL); // Obtiene el tiempo de inicio
         for (i=0; i < ARRAY_SIZE; i++) {
             c += m*a[i] + b[i];
         }
+        gettimeofday(&end_time, NULL); // Obtiene el tiempo de finalización
+        seconds = ((end_time.tv_sec * 1000000 + end_time.tv_usec) - (start_time.tv_sec * 1000000 + start_time.tv_usec)) * 1.0 / 1000000.0;
+        fprintf(pf, "%f %d\n", seconds, t);
     }
+    fclose(pf);
     printf("c = %f\n", c);
     return 0;
 }
--- P1/material/simple2_intrinsics.c
+++ P1/material/simple2_intrinsics.c
@@ -3,9 +3,10 @@
 #include <x86intrin.h>
 #include <xmmintrin.h>
 #include <immintrin.h>
+#include <sys/time.h>
 
 #define ARRAY_SIZE 2048
-#define NUMBER_OF_TRIALS 1
+#define NUMBER_OF_TRIALS 1000000
 
 /*
  * Statically allocate our arrays.  Compilers can
@@ -16,9 +17,10 @@ static double a[ARRAY_SIZE], b[ARRAY_SIZE], c;
 int main(int argc, char *argv[])
 {
     int i, j , t;
-
+    time_t t1, t2;
     double m = 1.0001;
-
+    struct timeval start_time, end_time;
+    float seconds=0;
 
     int diff = ARRAY_SIZE % 4;
     /* Populate A and B arrays */
@@ -39,9 +41,19 @@ int main(int argc, char *argv[])
    
     
     /* Perform an operation a number of times */
-    for (t = 0; t < NUMBER_OF_TRIALS; t++)
+
+    FILE *pf = fopen("tiempos_intrinseco.txt", "w");
+    if (!pf)
     {
-        for (i = 0; i < (ARRAY_SIZE-diff); i+=4)
+        if (pf)
+            fclose(pf);
+        return -1;
+    }
+
+    for (t = 100000; t < NUMBER_OF_TRIALS + 1; t+=100000)
+    {
+        gettimeofday(&start_time, NULL); // Obtiene el tiempo de inicio
+        for (i = 0; i < (ARRAY_SIZE - diff); i += 4)
         {
             __m256d sum = {0.0, 0.0, 0.0, 0.0};
             // Load arrays
@@ -55,16 +67,20 @@ int main(int argc, char *argv[])
             {
                 c += sum[j];
             }
+            
         }
-       
+
         for (i = (ARRAY_SIZE - diff); i < ARRAY_SIZE; i++)
         {
             c += m * a[i] + b[i];
         }
+        gettimeofday(&end_time, NULL); // Obtiene el tiempo de finalización
+        seconds = ((end_time.tv_sec * 1000000 + end_time.tv_usec) - (start_time.tv_sec * 1000000 + start_time.tv_usec)) * 1.0 / 1000000.0;
+        fprintf(pf, "%f %d\n", seconds, t);
     }
-   
-    printf("c = %f\n", c);
 
+    fclose(pf);
+    printf("c = %f\n", c);
 
     return 0;
 }
